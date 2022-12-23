93a5ff4a907e03c0885b410979c5fb4032506a56
Zlata Migas <zlatamigas@gmail.com>
2022-12-23 15:24:09 +0300

    NavigateUp on MainActivity

--- app/src/main/java/com/zlatamigas/readme/MainActivity.java
+++ app/src/main/java/com/zlatamigas/readme/MainActivity.java
@@ -22,6 +22,9 @@ public class MainActivity extends AppCompatActivity{
 
     private ActivityMainBinding binding;
 
+    private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +35,11 @@ public class MainActivity extends AppCompatActivity{
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_search, R.id.navigation_cart, R.id.navigation_mybooks, R.id.navigation_profile)
                 .build();
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
@@ -46,4 +49,8 @@ public class MainActivity extends AppCompatActivity{
         super.onBackPressed();
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp();
+    }
 }
